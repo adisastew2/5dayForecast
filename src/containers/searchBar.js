880ca33b37a6a825884f202ae8b08877e337d514
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-22 09:18:30 -0800

    Bind action creator fetchWeather to dispatch and map to props

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -23,7 +23,7 @@ export default class SearchBar extends Component {
 
   render () {
     return (
-      <form onSubmit = {onFormSubmit} className = "input-group" >
+      <form onSubmit = {this.onFormSubmit} className = "input-group" >
         <input 
           placeholder = "Input city name for 5 day forecast"
           className = "form-control"
@@ -37,3 +37,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
